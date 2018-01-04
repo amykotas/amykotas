f6667c1f7bf6feff7fd2ae4ac63d2248d9621e04
Amy Kotas <aekotas@gmail.com>
2018-01-04 17:50:16 -0500

    removed hover style on nav menu I previously changed from demo

--- wp-content/themes/workstation-pro/functions.php
+++ wp-content/themes/workstation-pro/functions.php
@@ -332,3 +332,10 @@ genesis_register_sidebar( array(
 	'name'        => __( 'Flexible Footer', 'workstation-pro' ),
 	'description' => __( 'This is the footer section.', 'workstation-pro' ),
 ) );
+
+// Stop images being wrapped in <p> tags.
+// credit http://www.wpfill.me/
+function filter_ptags_on_images($content){
+	return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+}
+add_filter('the_content', 'filter_ptags_on_images');
